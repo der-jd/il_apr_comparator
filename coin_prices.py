540544d8e8c5123168a53dc5eb8e1233bda4a50f
Julian <89513886+der-jd@users.noreply.github.com>
2023-03-27 21:18:09 +0200

    get coin prices from coingecko api

--- coin_prices.py
+++ coin_prices.py
@@ -4,23 +4,41 @@
 import datetime
 import requests
 
-import boto3
+
+# Coingecko API documentation: https://www.coingecko.com/en/api/documentation
+API_BASE_URL = "https://api.coingecko.com/api/v3"
 
 
-API_BASE_URL = "https://pro-api.coinmarketcap.com"
-AWS_SSM_NAME_API_KEY = "coinmarketcap-api-key"
+def get_coin_prices(coins: list[str], datetime_utc: datetime, currency: str = "eur") -> list[dict]:
+    print(f"Get coin prices for {coins} in {currency}...")
 
-
-def get_coin_prices(coins: list[str], date: datetime, currency: str = "EUR") -> dict:
-    if not date:
-        url = f"{API_BASE_URL}/v2/cryptocurrency/quotes/latest?symbol={','.join(c for c in coins)}&convert={currency}"
+    if not datetime_utc:
+        return _get_current_coin_prices(coins, currency)
     else:
-        url = f"{API_BASE_URL}/v1/cryptocurrency/quotes/historical?symbol={','.join(c for c in coins)}&convert={currency}" # TODO: not available for Basis plan --> need another API
+        prices = []
+        for c in coins:
+            data = _get_historical_coin_price(c, datetime_utc)
+            prices.append({c: {currency: data['market_data']['current_price'][data['symbol']]}})
+        return prices
 
+
+# coins: e.g. ["bitcoin", "ethereum", ...]
+# Example for response body:
+#{
+#  "bitcoin": {
+#    "eur": 25840
+#  },
+#  "ethereum": {
+#    "eur": 1636.82
+#  }
+#}
+def _get_current_coin_prices(coins: list[str], currency: str = "EUR") -> dict:
+    url = f"{API_BASE_URL}/simple/price?ids={','.join(c for c in coins)}&vs_currencies={currency}"
     headers = {
-        "Accept": "application/json",
-        "X-CMC_PRO_API_KEY": _get_api_key()
+        "accept": "application/json"
     }
+
+    print(f"Call {url}...")
     response = requests.get(url, headers = headers, timeout = 10)
 
     if response.status_code != 200:
@@ -29,6 +47,56 @@ def get_coin_prices(coins: list[str], date: datetime, currency: str = "EUR") -> 
     return response.json()
 
 
-def _get_api_key() -> str:
-    ssm_client = boto3.client('ssm')
-    return ssm_client.get_parameter(Name = AWS_SSM_NAME_API_KEY)
+# coin: e.g. "bitcoin" or "ethereum"
+# Example for response body:
+#{
+#  "id": "ethereum",
+#  "symbol": "eth",
+#  "name": "Ethereum",
+#  "localization": {
+#    "en": "Ethereum",
+#    "de": "Ethereum"
+#  },
+#  "image": {
+#    "thumb": "https://assets.coingecko.com/coins/images/279/thumb/ethereum.png?1595348880",
+#    "small": "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
+#  },
+#  "market_data": {
+#    "current_price": {
+#      "eur": 1127.1279368718483,
+#      ...
+#    },
+#    "market_cap": {
+#       ...
+#    },
+#    "total_volume": {
+#       ...
+#    }
+#  },
+#  "community_data": {
+#       ...
+#  },
+#  "developer_data": {
+#       ...
+#    },
+#    "commit_count_4_weeks": 29
+#  },
+#  "public_interest_stats": {
+#    "alexa_rank": null,
+#    "bing_matches": null
+#  }
+#}
+def _get_historical_coin_price(coin: str, datetime_utc: datetime) -> dict:
+    date = datetime_utc.datetime.strftime("%d-%m-%Y")
+    url = f"{API_BASE_URL}/coins/{coin}/history?date={date}"
+    headers = {
+        "accept": "application/json"
+    }
+
+    print(f"Call {url}...")
+    response = requests.get(url, headers = headers, timeout = 10)
+
+    if response.status_code != 200:
+        raise RuntimeError(f"ERROR: API call failed!\n{response.status_code} {response.reason}")
+
+    return response.json()
