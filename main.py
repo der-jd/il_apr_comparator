a7e90c566462b055b5cbed0bc70f721c48df4e84
Julian <89513886+der-jd@users.noreply.github.com>
2023-03-27 21:04:10 +0200

    added call to coin prices. used correct flag for case insensitive regex match

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
+import coin_prices
+
 
 URL_CAKEDEFI_LM = "https://app.cakedefi.com/liquidity-mining"
 REGEX_HTML_COIN_PAIR_BLOCK = ".*coinTextMedium.*"
@@ -38,13 +40,13 @@ def get_apr_from_cakedefi(coin_pair: tuple[str, str]) -> int:
     for i in coin_pair_blocks:
         # Alternative if pattern / approach. Just for documentation.
         # if i.find('span', {'class': re.compile(".*coinText.*")}, string = "BCH") and i.find('span', {'class': re.compile(".*coinText.*")}, string = "-DFI"):
-        if re.match(f"(?i){coin_pair[0]}\n?-\n?{coin_pair[1]}", i.get_text().strip()) or \
-           re.match(f"(?i){coin_pair[1]}\n?-\n?{coin_pair[0]}", i.get_text().strip()): # e.g. "BCH-DFI" or "DFI-BCH"
+        if re.match(f"{coin_pair[0]}\n?-\n?{coin_pair[1]}", i.get_text().strip(), re.IGNORECASE) or \
+           re.match(f"{coin_pair[1]}\n?-\n?{coin_pair[0]}", i.get_text().strip(), re.IGNORECASE): # e.g. "BCH-DFI" or "DFI-BCH"
             print(f"Requested coin pair '{coin_pair[0]}-{coin_pair[1]}' found!")
             print("Search for corresponding APR...")
             apr_block = i.find_next('div', {'class': re.compile(REGEX_HTML_APR_BLOCK)})
 
-            if re.match(r"\bAPR[0-9]?", apr_block.get_text().strip()): # (?i): case insensitive; \b: match beginning of a word
+            if re.match(r"\bAPR[0-9]?", apr_block.get_text().strip()): # \b: match beginning of a word
                 return re.search(r"[0-9]+\.[0-9]+", apr_block.get_text().strip()).group(0)
             else:
                 raise RuntimeError(f"ERROR: APR of coin pair '{coin_pair[0]}-{coin_pair[1]}' not found!")
@@ -53,4 +55,5 @@ def get_apr_from_cakedefi(coin_pair: tuple[str, str]) -> int:
 
 
 if __name__ == "__main__":
-   print(get_apr_from_cakedefi(("BCH", "DFI")))
+    print(get_apr_from_cakedefi(("BCH", "DFI")))
+    #coin_prices.get_coin_prices(, "eur")
