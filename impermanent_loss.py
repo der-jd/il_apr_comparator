8a4e8a6b610fe53f5e052e47a5c61e4867a9e659
Julian <89513886+der-jd@users.noreply.github.com>
2023-03-27 23:06:37 +0200

    fixed pyright errors

--- impermanent_loss.py
+++ impermanent_loss.py
@@ -4,7 +4,7 @@
 from math import sqrt
 
 
-def calculate_impermanent_loss(coin1_initial_price, coin2_initial_price, coin1_current_price, coin2_current_price):
+def calculate_impermanent_loss(coin1_initial_price: float, coin2_initial_price: float, coin1_current_price: float, coin2_current_price: float) -> float:
     print(f"Initial price coin 1: {coin1_initial_price}")
     print(f"Initial price coin 2: {coin2_initial_price}")
     print(f"Current price coin 1: {coin1_current_price}")
