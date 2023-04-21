a4e03ee0781ea69e8feeebe7472c91065d1670a5
Julian <89513886+der-jd@users.noreply.github.com>
2023-04-22 00:44:39 +0200

    Revert "try to fix error: use absolute file path for subprocess"
    This reverts commit e31109b86f1a5612e9b96fce66f3b2cfdf252e61.

--- aws/deploy_stack.py
+++ aws/deploy_stack.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
 import subprocess
 
 import boto3
@@ -12,7 +11,6 @@ import boto3
 # in combination with the corresponding "--no-fail-on-empty-changeset" option.
 def deploy_stack(template_file: str, stack_name: str) -> None:
     print(f"Deploy stack '{stack_name}'...")
-    template_file = os.path.abspath(template_file)
     command = f"aws cloudformation deploy --template-file {template_file} --stack-name {stack_name} --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset"
     result = subprocess.run(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE, check = False, text = True, shell = True)
     print(result.stdout, result.stderr)
