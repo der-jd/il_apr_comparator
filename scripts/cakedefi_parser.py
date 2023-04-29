f5241d2b1896dd23d06a174f67f94290e0c4dff7
Julian <89513886+der-jd@users.noreply.github.com>
2023-04-29 17:42:41 +0200

    use chrome instead of chromium

--- scripts/cakedefi_parser.py
+++ scripts/cakedefi_parser.py
@@ -17,9 +17,13 @@ def get_apr_from_cakedefi(coin_pair: tuple[str, str]) -> float:
     print(f"Get HTML content of {URL_CAKEDEFI_LM}...")
 
     chrome_options = ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(chrome_options = chrome_options)
+    #chrome_options.binary_location = "/usr/bin/chromium-browser"
+    #chrome_options.add_argument('--no-sandbox')
+    #chrome_options.add_argument("--disable-gpu")
+    chrome_options.headless = True
+    #chrome_options.add_argument('--disable-dev-shm-usage')
+    #driver = webdriver.Chrome('/usr/bin/local/chromedriver', options = chrome_options)
+    driver = webdriver.Chrome(options = chrome_options)
 
     driver.get(URL_CAKEDEFI_LM)
     try:
