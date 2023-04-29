bb3439d4642b1d1972c73490d948416dd4e4f913
Julian <89513886+der-jd@users.noreply.github.com>
2023-04-29 11:30:06 +0200

    added optional argument to use a parameters

--- aws/deploy_stack.py
+++ aws/deploy_stack.py
@@ -9,12 +9,16 @@ import boto3
 # This function should not fail on an empty changeset but instead just do nothing.
 # Therefor we need to use the AWS cli here because boto3 doesn't support the "deploy" command
 # in combination with the corresponding "--no-fail-on-empty-changeset" option.
-def deploy_stack(template_file: str, stack_name: str, parameters: list[str]) -> None:
+def deploy_stack(template_file: str, stack_name: str, parameters: list[str], parameters_file: str) -> None:
     print(f"Deploy stack '{stack_name}'...")
+
     if parameters:
         parameter_option = f" --parameter-overrides {' '.join(parameters)}"
+    elif parameters_file:
+        parameter_option = f" --parameter-overrides file://{parameters_file}"
     else:
         parameter_option = ""
+
     command = ''.join([
         "aws cloudformation deploy",
         f" --template-file {template_file}",
@@ -24,6 +28,7 @@ def deploy_stack(template_file: str, stack_name: str, parameters: list[str]) -> 
         " --no-fail-on-empty-changeset"])
     print(f"Run '{command}'...")
     result = subprocess.run(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE, check = False, text = True, shell = True)
+
     print(result.stdout, result.stderr)
     if result.returncode != 0:
         cloudformation = boto3.client('cloudformation')
@@ -32,18 +37,23 @@ def deploy_stack(template_file: str, stack_name: str, parameters: list[str]) -> 
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = "Create or update an AWS CloudFormation stack for a given template file, a stack name and optional parameters.")
+    parser = argparse.ArgumentParser(description = "Create or update an AWS CloudFormation stack for a given template file, " +
+                                                    "a stack name and optional parameters or a parameters file.")
     parser.add_argument('-t', '--template',
                         help = "name/path of the template file",
                         required = True)
     parser.add_argument('-s', '--stack_name',
                         help = "name of the stack",
                         required = True)
-    parser.add_argument('-p', '--parameters',
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-p', '--parameters',
                         help = "parameters as key-value-pairs separated by space, e.g. 'key1=value1 key2=value2 ...'",
                         nargs = '*',
                         action = 'extend',
                         required = False)
+    group.add_argument('-pf', '--parameters_file',
+                        help = "name/path of the parameters file in JSON format for the template",
+                        required = False)
     args = parser.parse_args()
 
-    deploy_stack(args.template, args.stack_name, args.parameters)
+    deploy_stack(args.template, args.stack_name, args.parameters, args.parameters_file)
