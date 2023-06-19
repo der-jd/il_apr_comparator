8fda482cb93014f945d1c395ca5896c3cc4fa176
Julian <89513886+der-jd@users.noreply.github.com>
2023-06-19 23:21:53 +0200

    fixed errors: removed unnecessary input data, fixed access of json data

--- app/cakedefi_parser.py
+++ app/cakedefi_parser.py
@@ -6,8 +6,6 @@ import aws
 
 # Scrape CakeDefi with https://www.browse.ai/
 
-URL_CAKEDEFI_LM = "https://app.cakedefi.com/liquidity-mining"
-COIN_PAIRS_MAX_NUMBER_OF_ROWS = 10
 ROBOT_LIST_NAME = "Coin_Pairs_LM_APR"
 ROBOT_LIST_NAME_COINPAIR_COLUMN = "coin_pair"
 API_BASE_URL = "https://api.browse.ai/v2" # API docu: https://www.browse.ai/docs/api/v2
@@ -16,7 +14,7 @@ API_BASE_URL = "https://api.browse.ai/v2" # API docu: https://www.browse.ai/docs
 def get_aprs_from_cakedefi() -> list[dict]:
     print("Use browse.ai to get the APR values from CakeDefi...")
     data = _run_robot()
-    data = _retrieve_task(data['id'])
+    data = _retrieve_task(data['result']['id'])
 
     result = []
     for pair in data['result']['capturedLists'][ROBOT_LIST_NAME]:
@@ -47,16 +45,10 @@ def _run_robot() -> dict:
     headers = {
         "Authorization": f"Bearer {api_key}"
     }
-    data = {
-        "inputParameters": {
-            "originUrl": URL_CAKEDEFI_LM,
-            "coin_pairs_lm_apr_limit": COIN_PAIRS_MAX_NUMBER_OF_ROWS
-        }
-    }
 
     print(f"Call {url}...")
     try:
-        response = requests.post(url, headers = headers, data = data, timeout = 10)
+        response = requests.post(url, headers = headers, timeout = 10)
         response.raise_for_status()
     except (requests.exceptions.RequestException, ValueError) as err:
         raise RuntimeError(f"ERROR: API call to start browse.ai robot failed!\n{str(err)}") from err
@@ -81,10 +73,11 @@ def _retrieve_task(task_id: str) -> dict:
             response.raise_for_status()
             data = response.json()
 
-            if data['status'] == "failed":
+            if data['result']['status'] == "failed":
                 raise RuntimeError(f"ERROR: Task of the browse.ai robot failed!\n{response.status_code} {response.reason}")
 
-            if data['status'] == "successful":
+            if data['result']['status'] == "successful":
+                print("Task finished successfully!")
                 return data
         except (requests.exceptions.RequestException, ValueError) as err:
             raise RuntimeError(f"ERROR: API call to retrieve task of the browse.ai robot failed!\n{str(err)}") from err
