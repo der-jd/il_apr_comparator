7e357afebaa41f5fe3c6743d8586bcecb10b7f82
Julian <89513886+der-jd@users.noreply.github.com>
2023-06-22 19:57:36 +0000

    only search for cryptocurrency pairs (no decentralized assets)

--- app/classic_scraping.py
+++ app/classic_scraping.py
@@ -16,6 +16,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 URL_BAKE_LM = "https://app.bake.io/liquidity-mining"
+REGEX_HTML_COIN_PAIRS_BLOCK = ".*cryptoPairTable.*"
 REGEX_HTML_COIN_PAIR_BLOCK = ".*coinTextMedium.*"
 REGEX_HTML_APR_BLOCK = ".*tableCellLeft.*tableCell.*"
 
@@ -41,7 +42,8 @@ def get_apr() -> list[dict]:
     html = driver.page_source
 
     soup = BeautifulSoup(html, "lxml")
-    coin_pair_blocks = soup.find_all('span', {'class': re.compile(REGEX_HTML_COIN_PAIR_BLOCK)})
+    block_with_cryptocurrencies = soup.find('div', {'class': re.compile(REGEX_HTML_COIN_PAIRS_BLOCK)})
+    coin_pair_blocks = block_with_cryptocurrencies.find_all('span', {'class': re.compile(REGEX_HTML_COIN_PAIR_BLOCK)})
     print(f"Found coin pairs:\n{[p.get_text().strip() for p in coin_pair_blocks]}")
 
     if not coin_pair_blocks:
