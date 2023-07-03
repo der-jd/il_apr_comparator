0a7a8167bb54eda011f24a1a10dc04a1e6357582
Julian <89513886+der-jd@users.noreply.github.com>
2023-07-03 21:07:36 +0200

    fixed formula for impermanent loss

--- app/impermanent_loss.py
+++ app/impermanent_loss.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
 
+# This function uses the Uniswap constant product formula to calculate the impermanent loss
 def calculate_impermanent_losses(coin_pairs: list[dict], initial_prices: dict, current_prices: dict, currency: str) -> dict:
     print("Calculate impermanent losses for cryptocurrency pairs...")
     result = {}
@@ -17,12 +18,38 @@ def calculate_impermanent_losses(coin_pairs: list[dict], initial_prices: dict, c
     return result
 
 
-# TODO fix il calculation. check which formula is used. there are different il formulas in the internet # pylint: disable = fixme
+# This function uses the Uniswap constant product formula to calculate the impermanent loss
+# To verify the result you can use this online calculator: https://dailydefi.org/tools/impermanent-loss-calculator/
 def _calculate_impermanent_loss(coin1_initial_price: float, coin2_initial_price: float, coin1_current_price: float, coin2_current_price: float) -> float:
     print(f"Initial price coin 1: {coin1_initial_price}")
     print(f"Initial price coin 2: {coin2_initial_price}")
     print(f"Current price coin 1: {coin1_current_price}")
     print(f"Current price coin 2: {coin2_current_price}")
-    initial_lp = 2 * sqrt(coin1_initial_price * coin2_initial_price) / (coin1_initial_price + coin2_initial_price)
-    current_lp = 2 * sqrt(coin1_current_price * coin2_current_price) / (coin1_current_price + coin2_current_price)
-    return current_lp / initial_lp - 1
+
+    # Explanation for calculation and formula: https://chainbulletin.com/impermanent-loss-explained-with-examples-math
+    # The formula to calculate impermanent loss in percent is:
+    # il_in_percent = (1 - stake_value/hold_value) * 100
+    #
+    # where:
+    # stake_value = Q1_current * coin1_current_price + Q2_current * coin2_current_price
+    #   with:
+    #       Q1 and Q2 as the quantity of coin 1 and 2 in the liquidity pool
+    #       Q1_pointintime = sqrt(k / r_pointintime)
+    #       Q2_pointintime = sqrt(k * r_pointintime)
+    #       k = Q1_pointintime * Q2_pointintime (constant product of quantities of coin 1 and 2)
+    #       r = coin1_pointintime_price / coin2_pointintime_price
+    #
+    # hold_value = Q1_initial * coin1_current_price + Q2_initial * coin2_current_price
+    #
+    # We can eliminate the coin quantities in the formula as they are irrelevant for the impermanent loss and they are also reflected by the corresponding coin prices.
+    # --> k = Q1_initial * Q2_initial = coin1_initial_price * coin2_initial_price
+    # and we can also replace 'Q1_initial' with 'coin2_initial_price' resp. 'Q2_initial' with 'coin1_initial_price'.
+    #
+    # ==> According to that the formula for impermanent loss is:
+
+    numerator = sqrt(coin1_initial_price * coin2_initial_price / (coin1_current_price / coin2_current_price)) * coin1_current_price + \
+                sqrt(coin1_initial_price * coin2_initial_price * (coin1_current_price / coin2_current_price)) * coin2_current_price
+    denominator = coin2_initial_price * coin1_current_price + coin1_initial_price * coin2_current_price
+    il_in_percent = (1 - numerator/denominator) * 100
+
+    return il_in_percent
